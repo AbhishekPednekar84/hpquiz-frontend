c13ba04818e72b434ceaa99cd8e02cd2e0311d77
Abhishek Pednekar <abhi_ap@hotmail.com>
2020-05-06 11:17:58 +0530

    Updated media query in App.css

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "./build")));
+app.use(express.static(path.join(__dirname, "./")));
 app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
 
 const PORT = process.env.PORT || 3000;
